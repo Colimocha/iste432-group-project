644464ee90feda9e808e8f14e9c3dc4614e74c5d
qwokwok <qpn6238@rit.edu>
2022-10-26 00:26:53 -0400

    set the login box to the center

--- front-nextjs/pages/login.js
+++ front-nextjs/pages/login.js
@@ -13,9 +13,13 @@ import {
   Snackbar,
   TextField,
   Typography,
+  ThemeProvider,
+  CssBaseline
 } from '@mui/material'
+
 import { useState } from 'react'
 import AuthServices from '../services/authServices'
+import { theme } from '../components/theme/Theme'
 
 export default function Login() {
   const [role, setRole] = useState(true)
@@ -39,16 +43,16 @@ export default function Login() {
 
     const data = role
       ? // Voter Login
-        {
-          cred_1: formData.get('credential1'),
-          cred_2: formData.get('credential2'),
-        }
+      {
+        cred_1: formData.get('credential1'),
+        cred_2: formData.get('credential2'),
+      }
       : // Dashboard Login
-        {
-          username: formData.get('username'),
-          password: formData.get('password'),
-          role: formData.get('role'),
-        }
+      {
+        username: formData.get('username'),
+        password: formData.get('password'),
+        role: formData.get('role'),
+      }
 
     // Send the data to the server.
     const response = role
@@ -66,104 +70,109 @@ export default function Login() {
   }
 
   return (
-    <Container
-      fixed
-      component="main"
-      className="w-full h-screen flex justify-center items-center"
-    >
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          backgroundColor: '#FFFFFF',
-          padding: 8,
-          borderRadius: 2,
-          width: '25em',
-        }}
-        className="shadow-2xl"
+    <ThemeProvider theme={theme}>
+      <Container
+        fixed
+        component="main"
+        className="w-full h-screen flex justify-center items-center"
+        maxWidth="xs"
       >
-        <Typography component="h1" variant="h5">
-          Sign in {role ? 'as a Voter' : 'Dashboard'}
-        </Typography>
+        <CssBaseline/>
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            backgroundColor: '#FFFFFF',
+            padding: 8,
+            borderRadius: 2,
+            width: '25em',
+          }}
+          className="shadow-2xl"
+        >
+          <Typography component="h1" variant="h5">
+            Sign in {role ? 'as a Voter' : 'Dashboard'}
+          </Typography>
 
-        <Box component="form" noValidate sx={{ mt: 4 }} onSubmit={handleSubmit}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id={role ? 'credential1' : 'username'}
-            label={role ? 'Credential #1' : 'Username'}
-            name={role ? 'credential1' : 'username'}
-            autoFocus
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id={role ? 'credential2' : 'password'}
-            label={role ? 'Credential #2' : 'Password'}
-            name={role ? 'credential2' : 'password'}
-            type={role ? 'text' : 'password'}
-          />
+          <Box component="form" noValidate sx={{ mt: 4 }} onSubmit={handleSubmit}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id={role ? 'credential1' : 'username'}
+              label={role ? 'Credential #1' : 'Username'}
+              name={role ? 'credential1' : 'username'}
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id={role ? 'credential2' : 'password'}
+              label={role ? 'Credential #2' : 'Password'}
+              name={role ? 'credential2' : 'password'}
+              type={role ? 'text' : 'password'}
+            />
 
-          {!role && (
-            <FormControl sx={{ mt: 2, minWidth: 120 }} fullWidth>
-              <InputLabel id="select-role">Role</InputLabel>
-              <Select
-                labelId="select-role"
-                id="selectRole"
-                label="Role"
-                name="role"
-              >
-                <MenuItem value={'societyContact'}>Society Contact</MenuItem>
-                <MenuItem value={'employee'}>Employee</MenuItem>
-              </Select>
-              <FormHelperText>Select the role</FormHelperText>
-            </FormControl>
-          )}
+            {!role && (
+              <FormControl sx={{ mt: 2, minWidth: 120 }} fullWidth>
+                <InputLabel id="select-role">Role</InputLabel>
+                <Select
+                  labelId="select-role"
+                  id="selectRole"
+                  label="Role"
+                  name="role"
+                >
+                  <MenuItem value={'societyContact'}>Society Contact</MenuItem>
+                  <MenuItem value={'employee'}>Employee</MenuItem>
+                </Select>
+                <FormHelperText>Select the role</FormHelperText>
+              </FormControl>
+            )}
 
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            className="bg-blue-500 hover:bg-blue-700"
-            sx={{ mt: 2, mb: 2, padding: 1.5 }}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              className="bg-blue-500 hover:bg-blue-700"
+              sx={{ mt: 2, mb: 2, padding: 1.5 }}
             // onClick={role ? handleVoterLogin : handleDashboardLogin}
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item>
-              <Link
-                onClick={role ? handleSwitchAdminLogin : handleSwitchVoterLogin}
-                className="cursor-pointer"
-                variant="body2"
-              >
-                Switch {role ? 'Dashboard' : 'Voter'}
-              </Link>
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link
+                  onClick={role ? handleSwitchAdminLogin : handleSwitchVoterLogin}
+                  className="cursor-pointer"
+                  variant="body2"
+                >
+                  Switch {role ? 'Dashboard' : 'Voter'}
+                </Link>
+              </Grid>
             </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
-      <Snackbar
-        open={openSuccess}
-        autoHideDuration={5000}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-      >
-        <Alert severity="success" sx={{ width: '100%' }}>
-          Login Successfully
-        </Alert>
-      </Snackbar>
-      <Snackbar
-        open={openError}
-        autoHideDuration={5000}
-        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-      >
-        <Alert severity="error" sx={{ width: '100%' }}>
-          Login Failed
-        </Alert>
-      </Snackbar>
-    </Container>
+        <Snackbar
+          open={openSuccess}
+          autoHideDuration={5000}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        >
+          <Alert severity="success" sx={{ width: '100%' }}>
+            Login Successfully
+          </Alert>
+        </Snackbar>
+        <Snackbar
+          open={openError}
+          autoHideDuration={5000}
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        >
+          <Alert severity="error" sx={{ width: '100%' }}>
+            Login Failed
+          </Alert>
+        </Snackbar>
+      </Container>
+    </ThemeProvider>
   )
 }
