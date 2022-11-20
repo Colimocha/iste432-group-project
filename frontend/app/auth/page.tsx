98300e333f0f61337227a3251b7a9699dbfc5951
Xiangyu Shi <xs8463@rit.edu>
2022-11-20 13:56:52 -0500

    updated auth stuff

--- frontend/app/auth/page.tsx
+++ frontend/app/auth/page.tsx
@@ -1,9 +1,46 @@
+'use client';
+
+import { useRef, useState } from 'react';
+
 export default function AuthPage() {
+  const [isVoter, setIsVoter] = useState(false);
+  const roleRef = useRef<HTMLSelectElement | null>(null);
+  const input_1_Ref = useRef<HTMLInputElement | null>(null);
+  const input_2_Ref = useRef<HTMLInputElement | null>(null);
+  const rememberMeRef = useRef<HTMLInputElement | null>(null);
+
+  const handleSelectRoleChange = () =>
+    setIsVoter(roleRef.current?.value === 'v');
+
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    const role = roleRef.current?.value;
+    const bodyForm =
+      role === 'v'
+        ? {
+            credential_1: input_1_Ref.current?.value,
+            credential_2: input_2_Ref.current?.value,
+            remember_me: rememberMeRef.current?.checked,
+          }
+        : {
+            username: input_1_Ref.current?.value,
+            password: input_2_Ref.current?.value,
+            remember_me: rememberMeRef.current?.checked,
+          };
+
+    //* Debug
+    console.log(bodyForm);
+
+    // TODO: Send bodyForm to server
+
+    // TODO: Redirect to home page for specific role
+  };
+
   return (
     <>
-      <div className="bg-gray-200 p-5 rounded-md shadow-lg lg:p-10">
-        <h1 className="text-4xl font-bold mb-6">Sign In</h1>
-        <form className="mt-3 space-y-6 w-[400px]">
+      <div className="rounded-md bg-gray-200 p-5 shadow-lg lg:p-10">
+        <h1 className="mb-6 text-4xl font-bold">Sign In</h1>
+        <form className="mt-3 w-[400px] space-y-6">
           {/* select role */}
           <div>
             <label htmlFor="role" className="sr-only">
@@ -14,62 +51,69 @@ export default function AuthPage() {
               name="role"
               autoComplete="role"
               required
-              className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+              className="select w-full"
               placeholder="Role"
+              ref={roleRef}
+              onChange={handleSelectRoleChange}
+              defaultValue=""
             >
-              <option value="" selected disabled>
+              <option value="" disabled>
                 None
               </option>
-              <option value="voter">Voter</option>
-              <option value="user">Society Contact</option>
-              <option value="admin">Employee</option>
+              <option value="v">Voter</option>
+              <option value="sc">Society Contact</option>
+              <option value="e">Employee</option>
             </select>
           </div>
           {/* Username */}
           <div>
-            <label htmlFor="username" className="sr-only">
-              Username
+            <label
+              htmlFor={isVoter ? 'credential_1' : 'username'}
+              className="sr-only"
+            >
+              {isVoter ? 'Credential 1' : 'Username'}
             </label>
             <input
-              id="username"
-              name="username"
+              id={isVoter ? 'credential_1' : 'username'}
+              name={isVoter ? 'credential_1' : 'username'}
               type="text"
-              autoComplete="username"
+              autoComplete={isVoter ? 'credential_1' : 'username'}
               required
-              className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-              placeholder="Username"
+              className="input w-full"
+              placeholder={isVoter ? 'Credential 1' : 'Username'}
+              ref={input_1_Ref}
             />
           </div>
           {/* Password */}
           <div>
-            <label htmlFor="password" className="sr-only">
-              Password
+            <label
+              htmlFor={isVoter ? 'credential_2' : 'password'}
+              className="sr-only"
+            >
+              {isVoter ? 'Credential 2' : 'Password'}
             </label>
             <input
-              id="password"
-              name="password"
-              type="password"
+              id={isVoter ? 'credential_2' : 'password'}
+              name={isVoter ? 'credential_2' : 'password'}
+              type={isVoter ? 'text' : 'password'}
               autoComplete="current-password"
               required
-              className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-              placeholder="Password"
+              className="input w-full"
+              placeholder={isVoter ? 'Credential 2' : 'Password'}
+              ref={input_2_Ref}
             />
           </div>
 
           {/* Remember Me and Forget Password */}
           <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember-me"
-                name="remember-me"
-                type="checkbox"
-                className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-              />
-              <label
-                htmlFor="remember-me"
-                className="ml-2 block text-sm text-gray-900"
-              >
-                Remember me
+            <div className="form-control">
+              <label className="label cursor-pointer">
+                <input
+                  type="checkbox"
+                  className="checkbox"
+                  ref={rememberMeRef}
+                />
+                <span className="label-text ml-2">Remember me</span>
               </label>
             </div>
             <div className="text-sm">
@@ -82,11 +126,7 @@ export default function AuthPage() {
             </div>
           </div>
           <div>
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
+            <button className="btn-primary btn w-full" onClick={handleSubmit}>
               Sign in
             </button>
           </div>
