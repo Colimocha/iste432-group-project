cf89a55d5bb4dbf2c13ca362f78719dad22d98ed
Xiangyu Shi <xs8463@rit.edu>
2022-11-26 21:00:32 -0500

    fixed the bug in admin/layout

--- frontend/app/(admin)/layout.tsx
+++ frontend/app/(admin)/layout.tsx
@@ -1,12 +1,18 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 
 // TODO - pass in the user's role and use it to determine which page to redirect to
 export default function Layout({ children }: { children: ReactNode }) {
   const router = useRouter();
-  const role = sessionStorage.getItem('role');
+  const [role, setRole] = useState('');
+
+  useEffect(() => {
+    const role = sessionStorage.getItem('role');
+    setRole(role!);
+  }, []);
+
   const allowedRoles = ['society_contact', 'employee'];
 
   switch (role) {
