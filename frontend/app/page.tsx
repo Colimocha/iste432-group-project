e0bbbae3d15b72895a7f60ef61a484aced4ead64
Xiangyu Shi <xs8463@rit.edu>
2022-12-04 22:24:17 -0500

    change to redirect auth page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,5 @@
-import { getVoters } from '#/lib/api/voter';
-import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <>
-      <div className="flex min-h-screen flex-col items-center justify-center space-y-4">
-        <div className="btn-primary btn">Root Main Page</div>
-        <div className="btn-secondary btn">
-          <Link href="/dashboard"> Dashboard </Link>
-        </div>
-        <div className="btn-secondary btn">
-          <Link href="/auth"> Auth </Link>
-        </div>
-      </div>
-    </>
-  );
+  return <>{redirect('/auth')}</>;
 }
